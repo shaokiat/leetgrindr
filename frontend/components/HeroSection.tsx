88456b23547671b3f69e1de95360d197b0a3d675
shaokiat <shaokiat@gmail.com>
2022-04-19 11:47:17 +0800

    update frontend UI

--- frontend/components/HeroSection.tsx
+++ frontend/components/HeroSection.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import astronaut from "../public/astronaut.png";
 import { useRouter } from "next/router";
 import { ChangeEvent, useState } from "react";
-import { createRoom } from "../services/room";
+import { createRoom, roomExist } from "../services/room";
 
 const HeroSection = () => {
   return (
     <div className="container mx-auto flex flex-col-reverse items-center lg:flex-row gap-6 pt-20 w-8/12 ">
-      <div className="lg:w-1/2 flex flex-col items-center lg:pt-20 ">
+      <div className="lg:w-2/3 flex flex-col items-center lg:pt-24 ">
         <HeroDescription />
         <HeroLogin />
       </div>
@@ -39,32 +39,39 @@ const HeroDescription = () => {
   );
 };
 
-type FormData = {
-  roomId: string;
-  name: string;
-};
-
 const HeroLogin = () => {
   const router = useRouter();
   const [roomId, setRoomId] = useState("");
   const [name, setName] = useState("");
   const [createRoomError, setCreateRoomError] = useState(false);
+  const [joinError, setJoinError] = useState(false);
 
   const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
     localStorage.setItem("name", name);
   };
 
-  const onSubmit = () => {
+  const onJoinRoomClick = async () => {
     if (roomId == "" && name == "") {
+      setJoinError(true);
       return;
     }
     // Search for room and enter existing room
+    const roomExists = roomExist(roomId);
+    if (!roomExists) {
+      setJoinError(true);
+      return;
+    } else {
+      router.push(`/room/${roomId}`);
+    }
   };
 
   const onCreateRoomClick = async () => {
+    if (name == "") {
+      setCreateRoomError(true);
+      return;
+    }
     const roomId = await createRoom();
-    console.log(roomId);
     if (!roomId) {
       setCreateRoomError(true);
       return;
@@ -73,63 +80,87 @@ const HeroLogin = () => {
   };
 
   return (
-    <form className="bg-slate-900 rounded px-8 pt-6 pb-8 mb-4 lg:w-8/12">
-      <div className="mb-6">
-        <label className="block text-gray-200 text-sm font-bold mb-2">
-          Name
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
-          id="name"
-          type="text"
-          placeholder="Name"
-          onChange={handleNameChange}
-        />
+    <div className="flex flex-col lg:flex-row lg:mt-10 w-full">
+      <form className="bg-slate-900 rounded px-8 pt-6 pb-8 mb-4 lg:w-5/12 w-full">
+        <div className="mb-6">
+          <label className="block text-gray-200 text-sm font-bold mb-2">
+            Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            type="text"
+            placeholder="Name"
+            onChange={handleNameChange}
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-200 text-sm font-bold mb-2">
+            Create New Room
+          </label>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+              onClick={onCreateRoomClick}
+            >
+              Create New Room
+            </button>
+          </div>
+        </div>
+        {createRoomError && (
+          <h1 className="text-red-400">
+            Sorry, something went wrong, please try again
+          </h1>
+        )}
+      </form>
+      <div className="flex justify-center align-middle lg:w-2/12 w-full mb-3">
+        <div className="text-gray-200 text-lg font-bold m-auto text-center">
+          OR
+        </div>
       </div>
-      <div className="mb-6">
-        <label className="block text-gray-200 text-sm font-bold mb-2">
-          Create New Room
-        </label>
-        <div className="flex items-center justify-between">
+      <form className="bg-slate-900 rounded px-8 pt-6 pb-8 mb-4 lg:w-5/12 w-full">
+        <div className="mb-6">
+          <label className="block text-gray-200 text-sm font-bold mb-2">
+            Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+            id="name"
+            type="text"
+            placeholder="Name"
+            onChange={handleNameChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-200 text-sm font-bold mb-2">
+            Room ID
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+            id="roomId"
+            type="text"
+            placeholder="Room ID"
+            onChange={(e) => setRoomId(e.target.value)}
+          />
+        </div>
+
+        <div className="flex items-center justify-between mb-5">
           <button
             className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="button"
-            onClick={onCreateRoomClick}
+            onClick={onJoinRoomClick}
           >
-            Create New Room
+            Join Room
           </button>
         </div>
-      </div>
-      <div className="block text-gray-200 text-sm font-bold mb-6">
-        OR join an existing room below!
-      </div>
-      <div className="mb-4">
-        <label className="block text-gray-200 text-sm font-bold mb-2">
-          Room ID
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
-          id="roomId"
-          type="text"
-          placeholder="Room ID"
-          onChange={(e) => setRoomId(e.target.value)}
-        />
-      </div>
-
-      <div className="flex items-center justify-between">
-        <button
-          className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="button"
-        >
-          Join Room
-        </button>
-      </div>
-      {createRoomError && (
-        <h1 className="text-red-400">
-          Sorry, something went wrong, please try again
-        </h1>
-      )}
-    </form>
+        {joinError && (
+          <h1 className="text-red-400">
+            Room not found! Please enter a correct room number.
+          </h1>
+        )}
+      </form>
+    </div>
   );
 };
 
